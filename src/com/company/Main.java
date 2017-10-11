ef8c1f79d21781ef02d19232919d2f313f273840
zoubeidaNM <zoumenz@yahoo.com>
2017-10-11 13:41:18 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/* Program ProgramToYourself
+*  Zoubeida Triki,
+*  October 10th, 2017
+*/
+public class Main {
+
+    public static void main(String[] args) {
+
+        // write my name to the console
+        System.out.println("My name is Zoubeida Triki");
+        // write my age to the console
+        System.out.println("My age is 40. (not true....)");
+    }
+}
